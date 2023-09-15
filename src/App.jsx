3580ce3ae051c64367f8441cbe8bd24afd19664c
kaulmesanyam <kaulsanyam@gmail.com>
2023-09-16 00:17:19 +0530

    playing with react

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,48 @@
 import React from "react";
 
 function App() {
-  const [todoForToday, setTodoForToday] = React.useState({
-    title: "do leetcode",
-    description: "do atleast 2 querstions of leetcode",
-    id: 1
-  });
+  const [todos, setTodoForToday] = React.useState([
+    {
+      title: " 1 do leetcode",
+      description: "do atleast 2 querstions of leetcode",
+      id: 1
+    },
+    {
+      title: "2 do leetcode",
+      description: "do atleast 2 querstions of leetcode",
+      id: 1
+    },
+    {
+      title: "3 do leetcode",
+      description: "do atleast 2 querstions of leetcode",
+      id: 1
+    }
+  ]);
 
-  React.useEffect(() => {
-    console.log("render");
-      setInterval(() => {
-        setTodoForToday({
-          title: "do leetcode" + Math.random(),
-          description: "do atleast 2 querstions of leetcode",
-          id: 1
-        })
-      }, 1000)
-  },[]);
+  // React.useEffect(() => {
+  //   console.log("render");
+  //     setInterval(() => {
+  //       setTodoForToday({
+  //         title: "do leetcode" + Math.random(),
+  //         description: "do atleast 2 querstions of leetcode",
+  //         id: 1
+  //       })
+  //     }, 1000)
+  // },[]);
 
   return (
-    <div>
-      {todoForToday.title}
-      <br/>
-      {todoForToday.description}
-    </div>
+    <dev>
+      {
+        todos.map((todo) => {
+          return (<div>
+          {todo.title}
+          <br/>
+          {todo.description}
+          </div>)
+        })
+      }
+    </dev>
+    
   )
 
 }
