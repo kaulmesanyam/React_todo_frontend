fd2f157063d343b516ba6bf6dccf0887592e85c6
kaulmesanyam <kaulsanyam@gmail.com>
2023-09-23 22:54:34 +0530

    connected to backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,30 @@
 import React from "react";
 
 function App() {
-  const [todos, setTodoForToday] = React.useState([
-    {
-      title: " 1 do leetcode",
-      description: "do atleast 2 querstions of leetcode",
-      id: 1
-    },
-    {
-      title: "2 do leetcode",
-      description: "do atleast 2 querstions of leetcode",
-      id: 1
-    },
-    {
-      title: "3 do leetcode",
-      description: "do atleast 2 querstions of leetcode",
-      id: 1
-    }
-  ]);
+  const [todos, setTodos] = React.useState([]);
 
-  // React.useEffect(() => {
-  //   console.log("render");
-  //     setInterval(() => {
-  //       setTodoForToday({
-  //         title: "do leetcode" + Math.random(),
-  //         description: "do atleast 2 querstions of leetcode",
-  //         id: 1
-  //       })
-  //     }, 1000)
-  // },[]);
+  React.useEffect(() => {
+    fetch("http://localhost:3000/todos", {
+      method: "GET"
+    }).then((resp) => {
+      resp.json().then((data) => {
+        console.log(data);
+        setTodos(data);
+      })
+    });
+  }, []);
 
   return (
     <dev>
-      {
-        todos.map((todo) => {
-          return (<div>
-          {todo.title}
-          <br/>
-          {todo.description}
-          </div>)
-        })
-      }
-    </dev>
-    
+     {todos.map(todo => {
+      return <div>
+        {todo.title}
+        {todo.description}
+        <button>Delete</button>
+        <br/>
+      </div>
+     })}
+    </dev> 
   )
 
 }
